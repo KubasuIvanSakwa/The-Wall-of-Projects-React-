0b87455588574cbe1e45ce36e76222ba52baea09
KubasuIvanSakwa <kubasucuba2020@gmail.com>
2023-12-11 13:15:15 +0300

    breaking changes -update

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -5,7 +5,7 @@ function Content() {
         <section className="w-full pl-5 pt-6">
             <section className="w-[99%] h-[99%]">
                 <Outlet />
-            </section>
+        </section>
         </section>
     )
 }
